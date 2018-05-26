a53d86e73332f17c7369af8ddbc498c9857ac561
Pawan Kumar <pawan221b@gmail.com>
2018-05-26 19:03:06 +0530

    First message

---
+++ README.md
@@ -0,0 +1,48 @@
+# Flutter QRCode Scanner APP
+
+![Image](fd.png)
+
+### Show some :heart: and star the repo to support the project
+
+<img src="https://thumbs.gfycat.com/BountifulIdenticalBumblebee-size_restricted.gif">
+
+A new Flutter project.
+
+### Features
+
+* [x] Scan 2D barcodes
+* [x] Scan QR codes
+* [x] Control the flash while scanning
+* [x] Permission handling
+* [ ] Support multiple barcode libraries
+
+## Getting Started
+
+### Flutter Project
+
+* Add this to your package's pubspec.yaml file:
+
+  `dependencies: barcode_scan: "^0.0.4"`
+
+### Android
+
+For Android, you must do the following before you can use the plugin:
+
+* Add the camera permission to your AndroidManifest.xml
+
+  `<uses-permission android:name="android.permission.CAMERA" />`
+
+* Add the Barcode activity to your AndroidManifest.xml
+  `<activity android:name="com.apptreesoftware.barcodescan.BarcodeScannerActivity"/>`
+
+### iOS
+
+To use on iOS, you must add the the camera usage description to your Info.plist
+
+    <key>NSCameraUsageDescription</key>
+    <string>Camera permission is required for barcode scanning.</string>
+
+## Getting Started with Flutter
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() {
+    return new HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  String result = "Hey there !";
+
+  Future _scanQR() async {
+    try {
+      String qrResult = await BarcodeScanner.scan();
+      setState(() {
+        result = qrResult;
+      });
+    } on PlatformException catch (ex) {
+      if (ex.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          result = "Camera permission was denied";
+        });
+      } else {
+        setState(() {
+          result = "Unknown Error $ex";
+        });
+      }
+    } on FormatException {
+      setState(() {
+        result = "You pressed the back button before scanning anything";
+      });
+    } catch (ex) {
+      setState(() {
+        result = "Unknown Error $ex";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Scanner"),
+      ),
+      body: Center(
+        child: Text(
+          result,
+          style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.camera_alt),
+        label: Text("Scan"),
+        onPressed: _scanQR,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
